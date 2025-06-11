4bd3e9c2e6962e22195d11c5ef679e15949da95e
JiaminLo <jluo6565@conestogac.on.ca>
2025-06-10 23:48:26 -0400

    update print result in comment

--- data_type.py
+++ data_type.py
@@ -9,7 +9,7 @@ print("string"[-1]) #print the last character of the string: g
 print("string"[0:3]) #print the first 3 characters of the string: str
 print("string"[0:]) #print the whole string: string
 print("string"[:3]) #print the first 3 characters of the string:str
-print("string"[0:3:2]) #print the first 3 characters of the string with a step of 2: st?
+print("string"[0:3:2]) #print the first 3 characters of the string with a step of 2: sr
 print("string"[::-1]) #print the string in reverse: gnirts
 print("string"[::-2]) #print the string in reverse with a step of 2: git
-print("string"[::-3]) #print the string in reverse with a step of 3: gn
+print("string"[::-3]) #print the string in reverse with a step of 3: gr
