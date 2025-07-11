949e5d6bb00ef8f1e116a81aacf0d6c257ae6a7f
JiaminLo <jluo6565@conestogac.on.ca>
2025-07-11 00:07:40 -0400

    update scoreboard class field

--- D23_turtle_crossing_game/turtle-crossing-start/scoreboard.py
+++ D23_turtle_crossing_game/turtle-crossing-start/scoreboard.py
@@ -5,19 +5,18 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.level = 1
         self.penup()
         self.color("black")
-        self.score = 0
+        self.goto(-280,260)
         self.hideturtle()
-        self.update_score()
-        self.level=1
-    def update_score(self):
-        self.goto(0,260)
-        self.write(f"Score: {self.level}", align="center", font=FONT)
+        self.update_scoreboard()
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
     def increase_level(self):
         self.level += 1
-        self.clear()
-        self.update_score()
+        self.update_scoreboard()
     def game_over(self):
         self.goto(0,0)
         self.write("GAME OVER", align="center", font=FONT)
