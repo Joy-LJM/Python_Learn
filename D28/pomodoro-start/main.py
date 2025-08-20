66bd3f20d511e904565c4e880091c0218bd4bff1
JiaminLo <jluo6565@conestogac.on.ca>
2025-08-19 23:12:12 -0400

    D28: complete user interface

--- D28/pomodoro-start/main.py
+++ D28/pomodoro-start/main.py
@@ -25,7 +25,18 @@ canvas=Canvas(width=200,height=224,background=YELLOW,highlightthickness=0)
 tomato=PhotoImage(file="tomato.png")
 canvas.create_image(100,112,image=tomato)
 canvas.create_text(100,130,font=(FONT_NAME,35,"bold"),text="00:00",fill="white")
-canvas.pack()
+canvas.grid(column=1,row=1)
 
+label=Label(text="Timer",fg=GREEN,font=(FONT_NAME,20,"bold"),bg=YELLOW)
+label.grid(column=1,row=0)
+
+start_button=Button(text="Start",highlightthickness=0)
+start_button.grid(column=0,row=2)
+
+reset_button=Button(text="Reset",highlightthickness=0)
+reset_button.grid(column=2,row=2)
+
+check_mark=Label(text="✔", fg=GREEN,font=(FONT_NAME,16,"bold"),bg=YELLOW)
+check_mark.grid(column=1,row=3)
 window.mainloop()
 
