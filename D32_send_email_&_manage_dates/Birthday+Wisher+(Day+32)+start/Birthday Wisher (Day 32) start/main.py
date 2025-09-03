95ce04a9bcecd92deccda6c9a896a2ec6b6e8c44
JiaminLo <jluo6565@conestogac.on.ca>
2025-09-02 21:54:09 -0400

    D32: datetime module

--- D32_send_email_&_manage_dates/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ D32_send_email_&_manage_dates/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,9 +1,9 @@
 import smtplib
 
 # should open 2-step authentication
-app_psw="znzersqfjymthjwc"
-yahoo_email="joytestingpython@yahoo.com"
-gmail="joy481339@gmail.com"
+# app_psw="znzersqfjymthjwc"
+# yahoo_email="joytestingpython@yahoo.com"
+# gmail="joy481339@gmail.com"
 
 # connection=smtplib.SMTP("smtp.gmail.com",587)
 # connection.starttls()
@@ -11,7 +11,16 @@ gmail="joy481339@gmail.com"
 # connection.sendmail(from_addr=gmail,to_addrs=yahoo_email,msg="hello")
 # connection.close()
 
-with smtplib.SMTP("smtp.gmail.com",587) as connection:
-    connection.starttls()
-    connection.login(user=gmail,password=app_psw)
-    connection.sendmail(from_addr=gmail,to_addrs=yahoo_email,msg="Subject:Hello\n\nThis is the body of my email")
+# method 2
+# with smtplib.SMTP("smtp.gmail.com",587) as connection:
+#     connection.starttls()
+#     connection.login(user=gmail,password=app_psw)
+#     connection.sendmail(from_addr=gmail,to_addrs=yahoo_email,msg="Subject:Hello\n\nThis is the body of my email")
+
+import datetime as dt
+
+now=dt.datetime.now()
+print(now.year,now.month,now.day)
+print(now)
+date_of_birth=dt.datetime(year=1995,month=12,day=31)
+print(date_of_birth)
