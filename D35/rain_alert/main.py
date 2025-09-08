7e35fe35a886af44416e1edb04fdbbbe3c2ed752
JiaminLo <jluo6565@conestogac.on.ca>
2025-09-08 17:57:19 -0400

    D35: using twilio sending sms

--- D35/rain_alert/main.py
+++ D35/rain_alert/main.py
@@ -1,8 +1,11 @@
 import requests
+# from twilio.rest import Client
 
 API_KEY="9413021806979ce883dc89aaec876344"
 MY_LAG= 43.652382
 MY_LONG=-79.383736
+account_sid="__YOUR_TWILIO_ACCOUNT_ID__"
+auth_token="__YOUR_TWILIO_AUTH_TOKEN__"
 
 parameter={
     "lat":MY_LAG,
@@ -23,6 +26,13 @@ for item in lists:
    if int(condition_code)<700:
        will_rain=True
 if will_rain:
-    print("Bring an umbrella!")
+    # client=Client(account_sid, auth_token)
+    # message=client.messages.create(
+    #     body="It's going to rain today. Remember to bring an ☔️",
+    #     from="YOUR TWILIO VIRTUAL NUMBER",
+    #     to="YOUR TWILIO VERIFIED REAL NUMBER"
+    # )
+    # print(message.status)
+    print("bring an ☔️")
 
 print(response.status_code)
