e9394cbfeda52bd870ce356b49a8b6e5766cc227
Joy-LJM <2522702541@qq.com>
2026-01-25 19:04:07 -0500

    D48: selenium

---
+++ D48_selenium_webdriver_and_game_playing_bot/interaction.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+chrome_options=webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
+driver =webdriver.Chrome(options=chrome_options)
+
+driver.get("https://en.wikipedia.org/wiki/Main_Page")
+article_count=driver.find_element(By.CSS_SELECTOR,value="#articlecount a")
+# article_count.click()
+print(article_count.text)
+all_portals=driver.find_element(By.LINK_TEXT,value="Content portals")
+# all_portals.click()
+
+driver.find_element(By.CSS_SELECTOR,value="#p-search a").click()
+search=driver.find_element(By.NAME,value="search")
+search.send_keys("Python", Keys.ENTER)
